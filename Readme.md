1ea5688792d3c765dcf0f5457c85f0bc04e0a9c8
fengjinlong <18618166564@163.com>
2018-08-14 16:48:33 +0800

    asd

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## debofj
+####去抖函数  
+npm install debofj --save
+####用户停止操作后，delay时间后执行主体函数func
+debounce (func, delay)
---
+++ index.js
@@ -0,0 +1,13 @@
+const debofj =  function debounce (func, delay) {
+  let timer
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      func.apply(this, args)
+    }, delay)
+  }
+}
+
+export default debofj
